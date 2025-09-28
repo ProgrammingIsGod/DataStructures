49d9d8c332e46a99c3c5956216b11d0cc30e58b4
ProgrammingIsGod <attackv00v@gmail.com>
2025-09-28 17:59:20 +0900

    로컬 변경사항 저장

---
+++ data_structure/queue.py
@@ -0,0 +1,86 @@
+from .array import Array, _sentinel
+
+class QueueError(Exception):
+    pass
+
+class Queue:
+    def __init__(self, capacity):
+        self._data = Array(capacity)
+    
+    def enqueue(self, value):
+        if self.is_full():
+            raise QueueError("queue is full")
+        self._data.append(value)
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise QueueError("queue is empty")
+
+        first_value = self._data[0]
+        for i in range(len(self._data)-1):
+            self._data[i] = self._data[i + 1]
+        self._data[i + 1] = _sentinel
+
+        return first_value
+    
+    def peek(self):
+        if self.is_empty():
+            raise QueueError("queue is empty")
+        return self._data[0]
+    
+    def is_empty(self):
+        return len(self._data) == 0
+    
+    def is_full(self):
+        return len(self._data) == self._data.array_size()
+    
+    def __len__(self):
+        return len(self._data)
+    
+class Circular_Queue:
+    def __init__(self, capacity):
+        self._data = Array(capacity)
+        self._size = 0
+        self._front = 0
+        self._rear = 0
+
+    def enqueue(self, value):
+        if self.is_full():
+            raise QueueError("queue is full")
+        
+        self._data[self._rear] = value
+        if self._rear + 1 == self._data.array_size():
+            self._rear = 0
+        else:
+            self._rear += 1
+        self._size += 1
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise QueueError("queue is empty")
+        
+        first_value = self._data[self._front]
+        if self._front + 1 == self._data.array_size():
+            self._front = 0
+        else:
+            self._front += 1
+        self._size -= 1
+
+        return first_value
+        
+    def peek(self):
+        if self.is_empty():
+            raise QueueError("queue is empty")
+        return self._data[self._front]
+    
+    def is_empty(self):
+        return self._size == 0
+
+    def is_full(self):
+        return self._size == self._data.array_size()
+    
+    def __len__(self):
+        return self._size
+    
+
+
--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,4 +1,4 @@
-from .array import Array
+from .array import Array, _sentinel
 
 class StackError(Exception):
     pass
@@ -12,14 +12,20 @@ class Stack:
 
     def pop(self):
         if self.is_empty() == True:
-            raise IndexError()
+            raise StackError("stack is empty")
         return self._data.pop()
 
     def peek(self):
+        if self.is_empty() == True:
+            raise StackError("stack is empty")
         return self._data[len(self._data) - 1]
 
     def is_empty(self):
         return len(self._data) == 0 
 
+    def stack_size(self):
+        return self._data.array_size()
+
     def __len__(self):
         return len(self._data)
+    
